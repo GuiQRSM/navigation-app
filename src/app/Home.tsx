566c2d1e2d836c815c6e725a4ac9d04e0921a70f
GuiQRSM <quaresmagui20@gmail.com>
2025-08-26 12:00:34 -0300

    feat: implements useNavigation

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,13 +2,15 @@ import { ButtonRouter } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
 import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export function Home() {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
         <Title>Home</Title>
-        <ButtonRouter name="add-circle" />
+        <ButtonRouter name="add-circle" onPress={() => navigation.navigate('product')} />
       </Header>
     </View>
   );
--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,13 +1,15 @@
 import { ButtonRouter } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
+import { useNavigation } from '@react-navigation/native';
 import { View } from 'react-native';
 
 export function Product() {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
-        <ButtonRouter name="arrow-circle-left" />
+        <ButtonRouter name="arrow-circle-left" onPress={() => navigation.goBack()} />
         <Title>Product</Title>
       </Header>
     </View>
