0e815aa658869d9f212de622f88cad464a0248bb
GuiQRSM <quaresmagui20@gmail.com>
2025-09-08 11:51:16 -0300

    feat: StackRouter ato TabRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StackRouter } from './StackRoutes';
+import { TabRouter } from './BottonTabs';
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <StackRouter />
+      <TabRouter />
     </NavigationContainer>
   );
 }
