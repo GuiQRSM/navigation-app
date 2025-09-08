11d398078169ba43977f376dd2275f899f8c3291
GuiQRSM <quaresmagui20@gmail.com>
2025-09-08 09:50:57 -0300

    feat: call my flexible typing as a prop of product screen

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -3,13 +3,9 @@ import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
 import { useNavigation } from '@react-navigation/native';
 import { View } from 'react-native';
-import { StackRouterList } from '../routes/StackRoutes';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { StackRoutesProps } from '@/routes/StackRoutes';
 
-type Props = NativeStackScreenProps<StackRouterList, 'product'>;
-
-export function Product() {
-  const navigation = useNavigation();
+export function Product({ navigation }: StackRoutesProps<'product'>) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
