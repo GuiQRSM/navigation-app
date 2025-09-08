e1ee938e5b5c5efc723b68a79aacde0f2a2985f9
GuiQRSM <quaresmagui20@gmail.com>
2025-09-08 11:49:31 -0300

    feat: rename Buttonrouter and call TabRoutesProps

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,15 +1,16 @@
-import { ButtonRouter } from '@/components/ButtonIcon';
+import { ButtonIcon } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
 import { View } from 'react-native';
 import { StackRoutesProps } from '@/routes/StackRoutes';
+import { TabRoutesProps } from '@/routes/BottonTabs';
 
-export function Home({ navigation }: StackRoutesProps<'home'>) {
+export function Home({ navigation }: TabRoutesProps<'home'>) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
         <Title>Home</Title>
-        <ButtonRouter
+        <ButtonIcon
           name="add-circle"
           onPress={() => navigation.navigate('product', { id: '456' })}
         />
