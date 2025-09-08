eb9fbb78e4775fe4918caff2cbcaf128c172a31f
GuiQRSM <quaresmagui20@gmail.com>
2025-09-08 09:48:33 -0300

    feat: call my flexible typing as a prop of home screen

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,11 +2,9 @@ import { ButtonRouter } from '@/components/ButtonIcon';
 import { Header } from '@/components/Header';
 import { Title } from '@/components/Title';
 import { View } from 'react-native';
-import { StackRouterList } from '../routes/StackRoutes';
+import { StackRoutesProps } from '@/routes/StackRoutes';
 
-type Props = NativeStackScreenProps<StackRouterList, 'home'>;
-
-export function Home({ navigation }: Props) {
+export function Home({ navigation }: StackRoutesProps<'home'>) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
