bb46e01c51f8250f3a2331170bd8ec0f41c521e6
GuiQRSM <quaresmagui20@gmail.com>
2025-09-08 11:21:45 -0300

    feat: configure the bootom tabs file

---
+++ src/routes/BottonTabs.tsx
@@ -0,0 +1,22 @@
+import { Home } from '@/app/Home';
+import { Product } from '@/app/Product';
+import { createBottomTabNavigator, BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+
+//as props são undefined pq product screen não tem um parâmetro obrigatório
+export type TabRouterList = {
+  home: undefined;
+  product: undefined | { id: string };
+};
+
+export type TabRoutesProps<T extends keyof TabRouterList> = BottomTabScreenProps<TabRouterList, T>;
+
+const Tab = createBottomTabNavigator<TabRouterList>();
+
+export function TabkRouter() {
+  return (
+    <Tab.Navigator initialRouteName="home" screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="home" component={Home} />
+      <Tab.Screen name="product" component={Product} />
+    </Tab.Navigator>
+  );
+}
