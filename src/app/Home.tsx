79c79a4018bba93a29f744ba83d5d415deee4b49
GuiQRSM <quaresmagui20@gmail.com>
2025-09-09 11:34:25 -0300

    feat: add the toggleDrawer in the icon for call Drawer

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -4,11 +4,13 @@ import { Title } from '@/components/Title';
 import { View } from 'react-native';
 import { StackRoutesProps } from '@/routes/StackRoutes';
 import { TabRoutesProps } from '@/routes/BottonTabs';
+import { DrawerRoutes } from '@/routes/DrawerRoutes';
 
 export function Home({ navigation }: TabRoutesProps<'home'>) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
+        <ButtonIcon name="menu" onPress={() => navigation.toggleDrawer()} />
         <Title>Home</Title>
         <ButtonIcon
           name="add-circle"
